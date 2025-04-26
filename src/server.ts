fe894d84fd928f0fba1432fb236bf01814d84c74
rajputayus7 <ayush.rajput@applore.in>
2025-04-26 21:56:39 +0530

    new template added.

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { serverConfig } from './config';
 import v1Router from './routers/v1/index.router';
 import v2Router from './routers/v2/index.router';
-import { appErrorHandler, genericErrorHandler } from './middlewares/error.middleware';
+import {appErrorHandler,  genericErrorHandler } from './middlewares/error.middleware';
 import logger from './config/logger.config';
 import { attachCorrelationIdMiddleware } from './middlewares/correlation.middleware';
 const app = express();
